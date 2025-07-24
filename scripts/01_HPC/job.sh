db567ae4326dc5a1397af7ccd00753633d4db5b8
Shodai <68819024+shodaiinose@users.noreply.github.com>
2025-07-24 11:38:34 -0400

    Update job.sh

--- scripts/01_HPC/job.sh
+++ scripts/01_HPC/job.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --account=msph
-#SBATCH --job-name=m11
+#SBATCH --job-name=m00
 #SBATCH --mem-per-cpu=5G
-#SBATCH --time=4-00:00
-#SBATCH --array=5-14
+#SBATCH --time=5-00:00
+#SBATCH --array=14
 #SBATCH -N 1                     
-#SBATCH -c 4
+#SBATCH -c 8
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=X@cumc.columbia.edu
 
-x=1 # aprime
-y=1 # astar
+x=0 # aprime
+y=0 # astar
 
 Rscript main.R $x $y 
 
